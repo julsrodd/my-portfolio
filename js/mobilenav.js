7d7e9320f951430a8e91a69dcc5eec95ee8f18d7
Julianna <julianna.rodriguezd@gmail.com>
2020-06-19 11:05:06 -0400

    mobile nav updates

--- js/mobilenav.js
+++ js/mobilenav.js
@@ -1,9 +1,19 @@
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
+/*function myFunction() {
   var x = document.getElementById("mymobilenav");
   if (x.style.display === "block") {
     x.style.display = "none";
   } else {
     x.style.display = "block";
   }
+}*/
+
+/* Open when someone clicks on the span element */
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
 }
